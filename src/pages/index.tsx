bb085a4fe336defaa7356e8fc8114c9e37998d1a
Karmotrine <yuanrcurena@gmail.com>
2023-03-09 14:14:12 +0800

    chore: add tailwindcss
    - Add and configure tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function IndexPage() {
     return (
-        <>
-            <h1>Test</h1>
-        </>
+        <h1 className="text-2xl underline">
+            Hello world!
+        </h1>
     )
 }
