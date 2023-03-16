c44cef102397bc54449170f9d5e55887c56c6d68
Pilner <raileyvictuelles@gmail.com>
2023-03-16 16:42:03 +0800

    ADDED Initial Styles and Utility Components
    +Added `/src/styles/base/typography.css` to house typographies/fonts.
    +Added `/src/components/base/RoundButton.tsx` for Round Buttons.
    +Added color variables in `globals.css`.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import Container from '@/components/Container';
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
   return (
     <Container>
       <Component {...pageProps} />
