49f6dc4ee4e31f618b545418abb9152c7523d5db
Karmotrine <yuanrcurena@gmail.com>
2023-04-01 15:16:27 +0800

    feat(hero): add details to sneak peak hero
    - add details to the carousel
    - move the sneak peak hero's jsx code to ShowcaseCarousel.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/Container";
 import HeroCarousel from "@/components/Home/Hero/HeroCarousel";
 import ShowcaseCarousel from "@/components/Home/Hero/ShowcaseCarousel";
 import HomeNews from "@/components/Home/News";
+import { ArrowDown } from "@phosphor-icons/react";
 
 /** @TODO: Another carousel below <HeroCarousel />
  *  To track slides: (slide.track.details.rel)
@@ -14,21 +15,8 @@ export default function IndexPage() {
             <div className="min-h-[calc(100vh_-_rem)]">
                 <HeroCarousel />
             </div>
-            <div className="min-h-[100vh] overflow-hidden" style={{background: "radial-gradient(100% 418.39% at 100% 100%, #343534 0%, #1E5C3F 33.33%, #409C49 66.67%, #FFEC5C 100%)"}}>
-                <div className="inline-flex xs:flex-col-reverse py-[13.813rem] min-h-[550px] min-w-[1144px] px-[9.25rem]"> {/*  */}
-                    <div className="col-span-2 h-full mr-[4.063rem]"> {/*  */}
-                        <p className="text-[2.5rem] font-bold text-white">
-                            lorem ipsum
-                        </p>
-                        <p className="text-[1.5rem] mt-[2.625rem] text-white">
-                            Lorem ipsum dolor sit amet
-                        </p>
-                        <button className="mt-[3.75rem] mb-[7.75rem]">
-                            <span className="text-[1rem]">Learn more</span>
-                        </button>
-                    </div>
-                    <ShowcaseCarousel /> 
-                </div>   
+            <div className="flex items-center justify-center min-h-[100vh] overflow-hidden" style={{background: "radial-gradient(100% 418.39% at 100% 100%, #343534 0%, #1E5C3F 33.33%, #409C49 66.67%, #FFEC5C 100%)"}}>
+                <ShowcaseCarousel /> 
             </div>
             <div className="z-100 min-h-[400vh]">
                 <h1 className="text-2xl underline">
