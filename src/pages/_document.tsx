f348266cee7c7008ab2eb82ae69e6db3fa9991e2
Pilner <raileyvictuelles@gmail.com>
2023-04-03 21:52:18 +0800

    FIXED `<title>` should not be used in _document.js's `<Head>` +Moved `<title>` from `_document.tsx` to `_app.tsx`

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>TPG | The Programmers' Guild</title>
       </Head>
       <body>
         <Main />
