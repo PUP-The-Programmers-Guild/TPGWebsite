ce3c07db589a9ebafa8439379bbf95a2832553fd
Lemon1903 <99325731+Lemon1903@users.noreply.github.com>
2023-06-11 19:23:33 +0800

    refactored and cleaned news page to align the tech stack

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,78 +1,62 @@
 import RoundButton from "@/components/base/RoundButton";
-import styles from "../../styles/newspage/news.module.css";
 
 export default function NewsFeedPage() {
   return (
     <>
-      <div className="z-100">
-        <div className="container mx-auto align-middle">
-          <div className="flex h-[calc(100vh_-_3rem)]">
-            <div className="flex flex-1 items-center justify-center">
-              <div>
-                <p className={`${styles.NewsMetaInfo} text-2xl`}>
-                  JAN 24 2023 | Author
-                </p>
-                <p className="Headings-Titles mb-10">
-                  SPOTLIGHT STORY
-                  <br />
-                  re:CONNECT || re:KINDLE 2023
-                </p>
-                <p className={`${styles.NewsMetaInfo} mb-10`}>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Voluptates dignissimos reiciendis eaque voluptas a laudantium
-                  obcaecati fugiat cum expedita necessitatibus! Amet eaque
-                  officiis blanditiis ratione! Dolor voluptatum numquam sint,
-                  maxime natus, sapiente aliquam mollitia assumenda
-                  reprehenderit iusto laborum, quis odit accusantium? Reiciendis
-                  quas optio molestias quo nisi maxime! Aperiam, illum.
-                </p>
-                <RoundButton url="#" text="Read" />
-              </div>
-            </div>
-            <div className="flex flex-1 items-center justify-center">
-              <div>
-                <img
-                  src="https://picsum.photos/1200/800"
-                  className="float-right w-[95%]"
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
+      {/* Spotlight Story */}
+      <div className="container mx-auto grid grid-cols-2 gap-8 py-14">
+        {/* Spotlight Metadata */}
+        <div className="grid justify-items-start">
+          <p className="text-2xl font-light">JAN 24 2023 | Author</p>
+          <p className="Headings-Titles">
+            SPOTLIGHT STORY re:CONNECT || re:KINDLE 2023
+          </p>
+          <p className="my-10 font-light">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+            dignissimos reiciendis eaque voluptas a laudantium obcaecati fugiat
+            cum expedita necessitatibus! Amet eaque officiis blanditiis ratione!
+            Dolor voluptatum numquam sint, maxime natus, sapiente aliquam
+            mollitia assumenda reprehenderit iusto laborum, quis odit
+            accusantium? Reiciendis quas optio molestias quo nisi maxime!
+            Aperiam, illum.
+          </p>
+          <RoundButton url="#" text="Read" />
         </div>
+
+        {/* Spotlight Image */}
+        <img
+          className="my-auto"
+          src="https://picsum.photos/1200/800"
+          alt="the spotlight picture"
+        />
       </div>
-      <div className="z-100" id={styles.eventsaccomp}>
-        <div className="container mx-auto py-20">
-          <p className="Subheadings-1 pb-20 text-5xl text-white">Events</p>
-          <div className="flex">
+
+      {/* TODO: The styles of the events and accomplishments container is the same, maybe converge to only one? */}
+      {/* Events and Accomplishments */}
+      <div className="divide-y-4 divide-white bg-[#293c4b] text-white">
+        {/* Events */}
+        <div className="container mx-auto space-y-20 py-20">
+          <p className="Subheadings-1 text-5xl">Events</p>
+          <div className="flex gap-4">
             {newsDataEvents.map((event) => (
-              <div className="flex-1">
-                <div className="mx-auto w-[95%]">
-                  <img src={event.picture} className="mb-5 w-[100%]" alt="" />
-                  <p className="Subheadings-1 mb-10 text-3xl text-white">
-                    {event.title}
-                  </p>
-                  <RoundButton url="#" text="Read" />
-                </div>
+              <div className="grid justify-items-start">
+                <img src={event.picture} alt="" />
+                <p className="Subheadings-1 my-5 text-3xl">{event.title}</p>
+                <RoundButton url="#" text="Read" />
               </div>
             ))}
           </div>
         </div>
-        <div className={`${styles.line} m-auto`} />
-        <div className="container mx-auto py-20">
-          <p className="Subheadings-1 pb-20 text-5xl text-white">
-            Accomplishments
-          </p>
-          <div className="flex">
+
+        {/* Accomplishments */}
+        <div className="container mx-auto space-y-20 py-20">
+          <p className="Subheadings-1 text-5xl">Accomplishments</p>
+          <div className="flex gap-4">
             {newsDataAcc.map((event) => (
-              <div className="flex-1">
-                <div className="mx-auto w-[95%]">
-                  <img src={event.picture} className="mb-5 w-[100%]" alt="" />
-                  <p className="Subheadings-1 mb-10 text-3xl text-white">
-                    {event.title}
-                  </p>
-                  <RoundButton url="#" text="Read" />
-                </div>
+              <div className="grid justify-items-start">
+                <img src={event.picture} alt="" />
+                <p className="Subheadings-1 my-5 text-3xl">{event.title}</p>
+                <RoundButton url="#" text="Read" />
               </div>
             ))}
           </div>
@@ -83,64 +67,40 @@ export default function NewsFeedPage() {
 }
 
 // Sample Data to Simulate CMS Backend
-const newsDataMain = [
-  {
-    date: "JAN 24 2023",
-    title: "Spotlight Story re:Connect || re:Kindle 2023",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates dignissimos reiciendis eaque voluptas a laudantium obcaecati fugiat cum expedita necessitatibus! Amet eaque officiis blanditiis ratione! Dolor voluptatum numquam sint, maxime natus, sapiente aliquam mollitia assumenda reprehenderit iusto laborum, quis odit accusantium? Reiciendis quas optio molestias quo nisi maxime! Aperiam, illum.",
-    picture: "news/main1.png",
-  },
-  {
-    date: "FEB 24 2023",
-    title: "TPG Look Back",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates dignissimos reiciendis eaque voluptas a laudantium obcaecati fugiat cum expedita necessitatibus! Amet eaque officiis blanditiis ratione! Dolor voluptatum numquam sint, maxime natus, sapiente aliquam mollitia assumenda reprehenderit iusto laborum, quis odit accusantium? Reiciendis quas optio molestias quo nisi maxime! Aperiam, illum.",
-    picture: "news/main2.png",
-  },
-  {
-    date: "March 24 2023",
-    title: "TPG Membership 2023",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates dignissimos reiciendis eaque voluptas a laudantium obcaecati fugiat cum expedita necessitatibus! Amet eaque officiis blanditiis ratione! Dolor voluptatum numquam sint, maxime natus, sapiente aliquam mollitia assumenda reprehenderit iusto laborum, quis odit accusantium? Reiciendis quas optio molestias quo nisi maxime! Aperiam, illum.",
-    picture: "news/main3.png",
-  },
-];
-
 const newsDataEvents = [
   {
-    picture: "news/event1.png",
     title: "Event News #1",
+    picture: "news/event1.png",
   },
   {
-    picture: "news/event2.png",
     title: "Event News #2",
+    picture: "news/event2.png",
   },
   {
-    picture: "news/event3.png",
     title: "Event News #3",
+    picture: "news/event3.png",
   },
   {
-    picture: "news/event4.png",
     title: "Event News #4",
+    picture: "news/event4.png",
   },
 ];
 
 const newsDataAcc = [
   {
-    picture: "news/accomplishment1.png",
     title: "Accomplishment #1",
+    picture: "news/accomplishment1.png",
   },
   {
-    picture: "news/accomplishment2.png",
     title: "Accomplishment #2",
+    picture: "news/accomplishment2.png",
   },
   {
-    picture: "news/accomplishment3.png",
     title: "Accomplishment #3",
+    picture: "news/accomplishment3.png",
   },
   {
-    picture: "news/accomplishment4.png",
     title: "Accomplishment #4",
+    picture: "news/accomplishment4.png",
   },
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
-const { fontFamily } = require('tailwindcss/defaultTheme')
-
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: [ "./src/**/*.{js,ts,jsx,tsx}" ],
   theme: {
     extend: {
       fontFamily: {
-        tpgTitle: ['var(--font-neptune)',],
-        pupTitle: ['var(--font-optimusPrinceps'],
-        heading: ['var(--font-centuryGothic)',],
-        title: ['var(--font-centuryGothic)',],
-        twcentmt: ['var(--font-twCenMt)',],
-        biko: ['var(--font-biko'],
-        body: ['var(--font-karla)',],
-      }
+        tpgTitle: "var(--font-neptune)",
+        pupTitle: "var(--font-optimusPrinceps",
+        heading: "var(--font-centuryGothic)",
+        title: "var(--font-centuryGothic)",
+        twcentmt: "var(--font-twCenMt)",
+        biko: "var(--font-biko",
+        body: "var(--font-karla)",
+      },
     },
     variants: {
       display: ['responsive', 'group-hover', 'group-focus'],
