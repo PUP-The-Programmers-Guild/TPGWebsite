39621f1ed51a4bd40bb1be8c65f2f909e3fc9a2a
Lemon1903 <99325731+Lemon1903@users.noreply.github.com>
2023-08-12 19:15:05 +0800

    added the accordion reusable component
    removed an empty file added

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,6 +49,20 @@ module.exports = {
         biko: ["var(--font-biko"],
         body: ["var(--font-karla)"],
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
     borderRadius: {
       lg: "var(--radius)",
