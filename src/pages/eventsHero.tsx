fcd94a9ec7a095d37d1a5d926181c738884d307b
John Paul <113358424+Viadsss@users.noreply.github.com>
2023-09-14 15:28:06 +0800

    Add eventsHero.tsx for [Events] - Hero section

---
+++ src/pages/eventsHero.tsx
@@ -0,0 +1,34 @@
+export default function eventsHero() {
+  return (
+    <div className="z-100 min-h-screen bg-gray-500">
+      <div className="flex flex-col justify-center gap-20 px-60 pb-28 pt-60">
+        <div className="flex flex-col gap-10 text-white">
+          <h1 className="font-heading text-5xl font-bold">
+            Building Connections and Experiences through Tech & Beyond
+          </h1>
+          <p>
+            Empower your growth journey with TPG&apos;s diverse tech events.
+          </p>
+        </div>
+        <a
+          href="#"
+          className="text- flex max-w-fit flex-row items-center gap-1 bg-white px-6 py-3"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="19"
+            viewBox="0 0 12 19"
+            fill="none"
+          >
+            <path
+              d="M6 18.5L11.7735 8.5L0.226498 8.5L6 18.5ZM5 0.5L5 9.5L7 9.5L7 0.5L5 0.5Z"
+              fill="black"
+            />
+          </svg>
+          Explore Events
+        </a>
+      </div>
+    </div>
+  );
+}
