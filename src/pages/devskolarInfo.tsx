5bdfcb4cf6ba3b360ca4643b7e2678e438b6a6bd
John Paul <113358424+Viadsss@users.noreply.github.com>
2023-09-14 15:37:29 +0800

    Add devskolarInfo.tsx for [Be a Devskolar] - Info section

---
+++ src/pages/devskolarInfo.tsx
@@ -0,0 +1,55 @@
+export default function devskolarInfo() {
+  return (
+    <div className="relative grid min-h-screen content-center bg-[#6F5D5D]">
+      <div className="absolute z-0 grid h-full w-full content-center justify-end overflow-hidden bg-black/10">
+        <div className="relative flex flex-col px-2">
+          <div className="mb-1 flex flex-row justify-center gap-x-1">
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+          </div>
+          <div className="mb-1 flex flex-row justify-center gap-x-1">
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+          </div>
+          <div className="flex flex-row justify-center gap-x-1">
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-[8.25rem] w-[16.25rem] bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+          </div>
+        </div>
+        <div className="relative flex flex-col px-2">
+          <div className="mb-1 flex flex-row justify-center gap-x-1">
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-[16.25rem] bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+          </div>
+          <div className="mb-1 flex flex-row justify-center gap-x-1">
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+          </div>
+          <div className="mb-1 flex flex-row justify-center gap-x-1">
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+            <div className="h-32 w-32 bg-[#8a8a8a]"></div>
+          </div>
+        </div>
+      </div>
+      <div className="z-10 flex flex-col gap-8 px-20 py-10 text-white">
+        <h1 className="font-tpgTitle text-8xl">BE A DEVSKOLAR</h1>
+        <p className="max-w-prose">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
+}
