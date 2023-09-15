7e87d637fd150ef75efa25d63e828bb22c702137
datsudo <>
2023-09-15 16:10:40 +0800

    Update css properties

--- src/components/About/MissionVision.tsx
+++ src/components/About/MissionVision.tsx
@@ -3,16 +3,16 @@ const VisionContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`
 
 export default function MissionVision() {
   return (
-    <section className="flex px-[15rem] py-[7.5rem] gap-x-[4.56rem]">
+    <section className="flex px-60 py-[7.5rem] gap-x-[4.56rem]">
       {
         [["Mission", MissionContent], ["Vision", VisionContent]]
         .map(([title, content]) => (
-          <div key={title}>
-            <div className="flex items-start gap-x-[1.5rem] mb-[2.5rem]" key={title}>
+          <div>
+            <div className="flex items-start gap-x-6 mb-10" key={title}>
               <h2 className="font-heading text-4xl font-bold">{title}</h2>
 
               {/* TODO: Icon for mission/vision beside the title */}
-              <div className="w-[2.5rem] h-[2.5rem] border border-red-500"></div>
+              <div className="w-10 h-10 border border-red-500"></div>
             </div>
             <p className="font-body text-2xl">{content}</p>
           </div>
