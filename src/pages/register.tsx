134a05dc829f888c4258f81e44224ded79807833
John Mark Peroche <92581738+Java-rice@users.noreply.github.com>
2023-09-15 21:48:03 +0800

    Update register.tsx
    Import the Requirements.tsx

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "../components/Devskolar/Hero";
 import ProcessSection from "../components/Devskolar/Process";
+import RequirementSection from "../components/Devskolar/Requirements";
 
 export default function RegisterPage() {
   return (
