9468097529a2f2d5c1af6f1a1deb85bff247375b
Darylle Villanueva <76833632+datsudo@users.noreply.github.com>
2023-09-15 22:37:18 +0800

    Create new About page section: Mission and Vision (#29)
    * Add new About page section: Mission and Vision
    
    ---------
    
    Co-authored-by: datsudo <>

---
+++ src/components/About/MissionVision.tsx
@@ -0,0 +1,23 @@
+const MissionContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`
+const VisionContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`
+
+export default function MissionVision() {
+  return (
+    <section className="flex px-60 py-[7.5rem] gap-x-[4.56rem]">
+      {
+        [["Mission", MissionContent], ["Vision", VisionContent]]
+        .map(([title, content]) => (
+          <div key={title}>
+            <div className="flex items-start gap-x-6 mb-10">
+              <h2 className="font-heading text-4xl font-bold">{title}</h2>
+
+              {/* TODO: Icon for mission/vision beside the title */}
+              <div className="w-10 h-10 border border-red-500"></div>
+            </div>
+            <p className="font-body text-2xl">{content}</p>
+          </div>
+        ))
+      }
+    </section>
+  )
+}
