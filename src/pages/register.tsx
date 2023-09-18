29cddc4ebbeec8151b9b330dff06fe7d92a8555e
John Mark Peroche <92581738+Java-rice@users.noreply.github.com>
2023-09-18 22:34:07 +0800

    Added Process Section and Requirements Section
    * Added devskolar requirement and process section on the updated dev
    * Fix file organization/Type Error/Document Formatting

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,4 +1,6 @@
-import HeroSection from "../components/Devskolar/Hero";
+import HeroSection from "../components/Devskolar/Hero/Hero";
+import ProcessSection from "../components/Devskolar/Process/Process";
+import RequirementSection from "../components/Devskolar/Requirements/Requirements";
 
 export default function RegisterPage() {
   return (
@@ -8,6 +10,8 @@ export default function RegisterPage() {
         titleSecondPart="DEV"
         titleThirdPart="SKOLAR"
       />
+      <ProcessSection/> {/*Process Section*/}
+      <RequirementSection/>  {/*Requirement Section*/}
       {/* The rest of your page content goes here */}
     </div>
   );
