07d1a3eca04cbe9b18f8e5f1c43bc68e85bd9a9f
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2023-09-19 10:38:49 +0800

    Deleted wrong file

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,11 @@
 import MissionVision from "@/components/About/MissionVision"
 import Demographics from "@/components/About/Demographics"
-import Officers from "@/components/About/Officers"
 
 export default function AboutPage() {
   return (
     <>
       <MissionVision />
       <Demographics />
-      <Officers />
     </>
   )
 }
