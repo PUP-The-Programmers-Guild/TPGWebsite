48fda58f0c2093c04bcadf48190f311c171b18e3
Lemon <khentalba1@gmail.com>
2023-09-27 13:44:34 +0800

    replaced arbitraries with tw classes and moved AboutHero section to appropriate folder

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,20 +1,12 @@
+import AboutHero from "@/components/About/AboutHero";
 import Demographics from "@/components/About/Demographics";
 import MissionVision from "@/components/About/MissionVision";
 import Officers from "@/components/About/Officers";
-import ShowcaseCarousel from "@/components/Home/Hero/AboutHero";
 
 export default function AboutPage() {
   return (
     <>
-      <div
-        className=" flex min-h-[100vh] items-center justify-center overflow-hidden"
-        style={{
-          background:
-            "radial-gradient(100% 418.39% at 100% 100%, #343534 0%, #1E5C3F 33.33%, #409C49 66.67%, #FFEC5C 100%)",
-        }}
-      >
-        <ShowcaseCarousel />
-      </div>
+      <AboutHero />
       <MissionVision />
       <Demographics />
       <Officers />
