c13d0258a99471f918819d8f7ab5c96c7260db34
Shania Faith Ramos <117431758+shaniassi@users.noreply.github.com>
2023-09-27 12:02:01 +0800

    created a devskolar benefits section  (#35)

--- src/components/Devskolar/Hero/Hero.tsx
+++ src/components/Devskolar/Hero/Hero.tsx
@@ -1,28 +1,14 @@
-import React from "react";
 import HeroSteps from "./HeroSteps";
 
-interface HeroProps {
-  titleFirstPart: string;
-  titleSecondPart: string;
-  titleThirdPart: string;
-}
-
-const HeroSection: React.FC<HeroProps> = ({
-  titleFirstPart,
-  titleSecondPart,
-  titleThirdPart,
-}) => {
+export default function HeroSection() {
   return (
-    <div className="relative h-screen bg-white">
+    <div className="relative h-screen overflow-hidden bg-white">
       <div className="flex items-center justify-between">
         <div className="w-1/2 p-16">
           <h1 className="pt-20 text-4xl">
-            <span className="text-7xl text-black">{titleFirstPart} </span>{" "}
-            <br />
-            <span className="text-8xl font-bold text-green-500">
-              {titleSecondPart}
-            </span>
-            <span className="text-8xl font-bold">{titleThirdPart}</span>
+            <span className="text-7xl text-black">BE A </span> <br />
+            <span className="text-8xl font-bold text-green-500">DEV</span>
+            <span className="text-8xl font-bold">SKOLAR</span>
           </h1>
           <div className="py-4">
             <button className="mx-auto inline-block max-w-xs rounded-2xl bg-gray-800 px-4 py-2 text-white">
@@ -34,6 +20,4 @@ const HeroSection: React.FC<HeroProps> = ({
       <HeroSteps />
     </div>
   );
-};
-
-export default HeroSection;
+}
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,18 +1,15 @@
-import HeroSection from "../components/Devskolar/Hero/Hero";
-import ProcessSection from "../components/Devskolar/Process/Process";
-import RequirementSection from "../components/Devskolar/Requirements/Requirements";
+import DevskolarBenefits from "@/components/Devskolar/Benefits";
+import HeroSection from "@/components/Devskolar/Hero/Hero";
+import ProcessSection from "@/components/Devskolar/Process/Process";
+import RequirementSection from "@/components/Devskolar/Requirements/Requirements";
 
 export default function RegisterPage() {
   return (
-    <div>
-      <HeroSection
-        titleFirstPart="BE A"
-        titleSecondPart="DEV"
-        titleThirdPart="SKOLAR"
-      />
-      <ProcessSection/> {/*Process Section*/}
-      <RequirementSection/>  {/*Requirement Section*/}
-      {/* The rest of your page content goes here */}
-    </div>
+    <>
+      <HeroSection />
+      <DevskolarBenefits />
+      <ProcessSection />
+      <RequirementSection />
+    </>
   );
 }
