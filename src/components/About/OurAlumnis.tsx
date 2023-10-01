59c7f171c1033beab63b163010616ed95c60ac00
Shania Faith Ramos <117431758+shaniassi@users.noreply.github.com>
2023-10-01 12:47:13 +0800

    Update OurAlumnis.tsx

--- src/components/About/OurAlumnis.tsx
+++ src/components/About/OurAlumnis.tsx
@@ -1,22 +1,22 @@
 export default function OurAlumnisPage() {
   return (
-    <div className="frame182 relative flex flex-col gap-5 bg-[#8A8A8A] px-60 py-28">
-      <div className="frame187 relative flex flex-col gap-14">
-        <h1 className="subheading relative font-heading text-3xl font-bold text-white">
+    <div className="relative flex flex-col gap-5 bg-[#8A8A8A] px-60 py-28">
+      <div className="relative flex flex-col gap-14">
+        <h1 className="relative font-heading text-3xl font-bold text-white">
           Our Alumnis
         </h1>
-        <p className="subheading2 relative flex items-center self-stretch text-xl font-normal text-white">
+        <p className="relative flex items-center self-stretch text-xl font-normal text-white">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
           dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
           amet, consectetur adipiscing elit.
         </p>
-        <div className="frame185 relative flex flex-col gap-12 self-stretch">
-          <div className="frame184 relative flex flex-col gap-5 self-stretch">
-            <div className="frame181 relative flex flex-col gap-3">
-              <div className="_2022 relative w-32 font-heading text-2xl text-white">
+        <div className="relative flex flex-col gap-12 self-stretch">
+          <div className="relative flex flex-col gap-5 self-stretch">
+            <div className="relative flex flex-col gap-3">
+              <div className="relative w-32 font-heading text-2xl text-white">
                 2022
               </div>
-              <div className="group107 visible absolute left-20 top-2">
+              <div className="visible absolute left-20 top-2">
                 <svg
                   width="17"
                   height="9"
@@ -43,7 +43,7 @@ export default function OurAlumnisPage() {
                 </svg>
               </div>
             </div>
-            <div className="line1 visible relative shrink-0 translate-y-1 self-stretch overflow-visible rounded-none">
+            <div className="visible relative shrink-0 translate-y-1 self-stretch overflow-visible rounded-none">
               <svg
                 width="967"
                 viewBox="0 -2 967 10"
@@ -58,8 +58,8 @@ export default function OurAlumnisPage() {
               </svg>
             </div>
           </div>
-          <div className="frame186 relative ml-28 flex w-[840px] flex-row gap-16">
-            <p className="estrella relative flex-1 font-heading text-base text-white">
+          <div className="relative ml-28 flex w-[840px] flex-row gap-16">
+            <p className="relative flex-1 font-heading text-base text-white">
               Estrella Coleman
               <br />
               Warren Patrick
@@ -84,7 +84,7 @@ export default function OurAlumnisPage() {
               <br />
               Jasiah Schwartz
             </p>
-            <p className="katherine relative flex-1 font-heading text-base text-white">
+            <p className="relative flex-1 font-heading text-base text-white">
               Katherine Cox
               <br />
               Marely Pearson
@@ -109,7 +109,7 @@ export default function OurAlumnisPage() {
               <br />
               Yamilet Mcconnell
             </p>
-            <p className="katherine2 relative flex-1 font-heading text-base text-white">
+            <p className="relative flex-1 font-heading text-base text-white">
               Katherine Cox
               <br />
               Marely Pearson
