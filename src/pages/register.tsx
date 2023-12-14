0fb86d3fc28ee8c5fa1d4125b364d524e74514ac
Shania Faith Ramos <117431758+shaniassi@users.noreply.github.com>
2023-12-15 07:29:27 +0800

    created a section of the About page: our alumnis (#42)
    * added the devskolar benefits for 'be a devskolar' page
    
    * added 'Our Alumnis' section of 'About Us' page
    
    * Update OurAlumnis.tsx
    
    * refactor: minor file + code organization fixes
    
    ---------
    
    Co-authored-by: Karmotrine <yuanrcurena@gmail.com>

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,7 @@
-import DevskolarBenefits from "@/components/Devskolar/Benefits";
 import HeroSection from "@/components/Devskolar/Hero/Hero";
 import ProcessSection from "@/components/Devskolar/Process/Process";
 import RequirementSection from "@/components/Devskolar/Requirements/Requirements";
+import DevskolarBenefits from "@/components/Devskolar/Benefits/Benefits";
 
 export default function RegisterPage() {
   return (
