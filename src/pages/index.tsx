16963a2a6eaa334791b1edeba2eded43e4c41a13
Lemon <khentalba1@gmail.com>
2024-02-26 15:34:11 +0800

    added faqs section in home page (#45)
    Co-authored-by: Yuan <72153098+Karmotrine@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,6 @@ import HomeDemographics from "@/components/Home/HomeDemographics";
 import LatestNews from "@/components/Home/LatestNews/LatestNews";
 import Mission from "@/components/Home/Mission";
 
-/** @TODO: Another carousel below <HeroCarousel />
- *  To track slides: (slide.track.details.rel)
- *  example: https://codesandbox.io/s/friendly-kalam-8u4uec?file=/index.html
- */
-
 export default function IndexPage() {
   return (
     <>
