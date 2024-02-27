1db299b3750b9916de082e85cabca01ebcb819d1
Yuan <72153098+Karmotrine@users.noreply.github.com>
2024-02-28 01:28:32 +0800

    refactor: initial mi-fi impl... of mission comp.  (#61)
    - change image placeholder inside mission component to match mid-fidelity Figma design
    - change image placeholder's html element from img to next/Image component
    - minor adjustment to text colors and spacing of subelements
    - clear unused styles in global.css

--- src/components/Home/Mission.tsx
+++ src/components/Home/Mission.tsx
@@ -1,36 +1,39 @@
 import { ArrowUpRight } from "@phosphor-icons/react";
+import Image from "next/image";
 
 export default function Mission() {
   return (
-    <section className="flex min-h-screen items-center justify-center bg-[#8A8A8A]">
+    <section className="flex min-h-screen items-center justify-center bg-gradient-to-b from-[#164323] to-[#052014]">
       <div className="flex max-w-[960px] flex-col pb-[115px] pt-[70px]">
-        <div className="pb-[6rem] text-center">
-          <h1 className={`pb-[2rem] font-heading text-6xl font-bold leading-[84px] text-white `}>WHAT IS PUP - TPG?</h1>
+        <div className="pb-[100px] text-center">
+          <h1 className={`pb-[40px] font-heading text-6xl font-bold leading-[84px] text-[#E6F5D6] `}>
+            WHAT IS PUP - TPG?
+          </h1>
           <div className="mx-auto">
-            <p className="font-body text-xl leading-6 text-white">
+            <p className="font-body text-xl leading-6 text-[#E6F5D6]">
               The PUP Programmers&apos; Guild is a university-wide organization composed of different programmers and
               developers, conducting various events and activities related to the fields of programming and development.
             </p>
           </div>
         </div>
 
-        <div className="flex flex-row justify-between pb-[2.5rem]">
+        <div className="flex flex-row justify-between pb-[40px]">
           <div className="max-w-sm  lg:text-center ">
-            <h2 className={`pb-[25px] font-heading text-5xl font-bold leading-[67.2px] text-white`}>Our Mission</h2>
-            <p className="my-[29px] text-justify font-body text-xl leading-6 text-white">
+            <h2 className={`pb-[25px] font-heading text-5xl font-bold leading-[67.2px] text-[#E6F5D6]`}>Our Mission</h2>
+            <p className="my-[29px] text-justify font-body text-xl leading-6 text-[#E6F5D6]">
               A long-run objective of our organization that describes its day-to-day operations, including the values
               and public commitment to its members and the community.
             </p>
           </div>
-          <div className="relative order-first max-w-[493px] lg:order-last">
-            <img src="Placeholder1.png" alt="Placeholder" className="mt-[1rem] h-full w-full lg:mt-0" />
+          <div className="relative order-last w-[493px]">
+            <Image src="/AboutTPG/AboutTPGMission.webp" alt="Placeholder" fill className="h-full w-full lg:mt-0" />
           </div>
         </div>
 
-        <div className="flex items-center justify-center">
-          <button className="flex items-center justify-end gap-[10px] bg-white p-[20px] px-5 py-[10px] hover:bg-[#E6F5D6]">
-            <span className="font-body text-base font-bold">Learn more</span>
-            <ArrowUpRight size={24} weight="fill" />
+        <div className="flex items-center justify-center text-[#052014]">
+          <button className="flex items-center justify-end gap-[10px] bg-white p-[20px] py-[10px] pl-5 pr-4 hover:bg-[#E6F5D6]">
+            <span className="font-body text-base font-bold ">Learn More</span>
+            <ArrowUpRight size={22} weight="fill" />
           </button>
         </div>
       </div>
