974613549deadfd4c5bef4d659d6d621a61c984c
Karmotrine <yuanrcurena@gmail.com>
2024-02-27 23:46:53 +0800

    refactor: implement mid-fi design of Hero Carousel
    - change slideshow images of HeroCarousel to stock photos to match Figma design
    - add green hue gradient overlay of HeroCarousel
    - adjust position of CaretDown icon component of HeroCarousel
    - adjust text color to match figma mid-fidelity design

--- src/components/Home/HeroCarousel.tsx
+++ src/components/Home/HeroCarousel.tsx
@@ -6,12 +6,7 @@ import { useState } from "react";
 // Inf. Loop Move, Lazy load
 
 // Mock Data, pictures will be fetched from sanity
-const images = [
-  "https://images.unsplash.com/photo-1590004953392-5aba2e72269a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
-  "https://images.unsplash.com/photo-1590004845575-cc18b13d1d0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
-  "https://images.unsplash.com/photo-1590004987778-bece5c9adab6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
-  "https://images.unsplash.com/photo-1590005176489-db2e714711fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
-];
+const images = ["/hero/hero2.webp", "/hero/hero2.webp", "/hero/hero2.webp", "/hero/hero2.webp"];
 
 export default function HeroCarousel() {
   const [opacities, setOpacities] = useState<number[]>([]);
@@ -59,14 +54,15 @@ export default function HeroCarousel() {
 
   return (
     <section className="relative -z-10 min-h-[calc(100vh-80px)]">
-      <h1 className="absolute  left-1/2 top-[calc(50%-80px)] z-10 w-3/4  -translate-x-1/2 -translate-y-1/2 text-center font-heading text-8xl uppercase leading-[96.02px] text-white">
+      <h1 className="absolute left-1/2 top-[calc(50%-80px)] z-10 w-3/4  -translate-x-1/2 -translate-y-1/2 text-center font-heading text-8xl uppercase leading-[96.02px] text-[#E6F5D6]">
         Empowering the next generation of coders
       </h1>
-      <button className="absolute bottom-[calc(3%+80px)] left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
+      <button className="absolute bottom-[calc(3%+90px)] left-1/2 -translate-x-1/2 -translate-y-1/2 font-tpgTitle font-bold text-[#E6F5D6]">
         <CaretDown size={91} weight="bold" />
       </button>
 
       <div ref={sliderRef} className="relative -z-10 min-h-screen min-w-full overflow-hidden">
+        <div className="absolute z-40 h-full w-full bg-gradient-to-b from-[#052014] to-[#164323] opacity-80" />
         {images.map((src, idx) => (
           <div key={idx} className="absolute -z-10 min-h-full min-w-full" style={{ opacity: opacities[idx] }}>
             <Image
