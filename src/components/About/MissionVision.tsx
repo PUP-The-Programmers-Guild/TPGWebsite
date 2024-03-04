1b225c98b40d94bd60415ae86230345f67b8102d
Yuan <72153098+Karmotrine@users.noreply.github.com>
2024-03-04 19:08:14 +0800

    refactor: impl. mifi design for MissionVision comp (#68)
    - modify MissionContent, VisionContent to contain title, icon, content information
    - adjust section to match max width of 960px (navbar as basis)
    - add icon for "Mission" and "Vision" portions
    - add background image (public/AboutTPG/AboutTPGMission.webp) for the whole section element
    - apply radial gradient filter to the background image

--- src/components/About/MissionVision.tsx
+++ src/components/About/MissionVision.tsx
@@ -1,23 +1,39 @@
-const MissionContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`
-const VisionContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`
+import { LightbulbFilament, RocketLaunch } from "@phosphor-icons/react";
+import Image from "next/image";
+
+const MissionInfo = {
+  title: "Mission",
+  content: `Our mission is to be an online committee of curious and interested individuals who desires to innovate and create by utilizing IT, putting together great minds and building something that will be greater than they have imagined it to be.`,
+  icon: <RocketLaunch size={40} weight="fill" />,
+};
+const VisionInfo = {
+  title: "Vision",
+  content: `We envision ourselves to be a strong organization that enriches the ability of students to outstandingly perform activities and competitions in all fields of Information Technology together, towards the goal of building better bonds and beneficial output to the society.`,
+  icon: <LightbulbFilament size={40} weight="fill" />,
+};
 
 export default function MissionVision() {
   return (
-    <section className="flex px-60 py-[7.5rem] gap-x-[4.56rem]">
-      {
-        [["Mission", MissionContent], ["Vision", VisionContent]]
-        .map(([title, content]) => (
-          <div key={title}>
-            <div className="flex items-start gap-x-6 mb-10">
-              <h2 className="font-heading text-4xl font-bold">{title}</h2>
-
-              {/* TODO: Icon for mission/vision beside the title */}
-              <div className="w-10 h-10 border border-red-500"></div>
+    <section className="flex min-h-screen items-center justify-center gap-x-[4.56rem] px-[240px] py-[240px] text-white">
+      <div className="absolute -z-20 h-full w-full overflow-hidden bg-[radial-gradient(1718.21%_485.56%_at_-1%_463.56%,_#2A9134_0%,_#052014_85.26%)]">
+        <Image
+          src="/AboutTPG/AboutTPGMission.webp"
+          alt="TPG Mission-Vision Background Image"
+          fill
+          className="z-10 h-[800px] w-full translate-y-24  scale-[125%] object-cover opacity-20"
+        />
+      </div>
+      <div className="flex flex-row justify-between gap-x-[80px]">
+        {[VisionInfo, MissionInfo].map((info) => (
+          <div key={info.title}>
+            <div className="mb-[40px] flex items-start gap-x-6">
+              <h2 className="font-heading text-4xl font-bold">{info.title}</h2>
+              <div className="h-10 w-10 text-white">{info.icon}</div>
             </div>
-            <p className="font-body text-2xl">{content}</p>
+            <p className="font-body text-2xl leading-[36px]">{info.content}</p>
           </div>
-        ))
-      }
+        ))}
+      </div>
     </section>
-  )
+  );
 }
