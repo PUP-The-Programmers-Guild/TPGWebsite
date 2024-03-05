68be1fd82da45d525309eeca79b744135192a20c
Karmotrine <yuanrcurena@gmail.com>
2024-03-05 14:19:49 +0800

    refactor: impl. mifi for about us demographics
    - set section content max-width to 960px, align to center horizontally and vertically
    - update section background color to tpg green
    - update text content sizing, text container spacing and gaps
    - update text content according to Mid-Fidelity designs from Figma

--- src/components/About/Demographics.tsx
+++ src/components/About/Demographics.tsx
@@ -1,30 +1,33 @@
 const demographics = [
-  ["Members", "+200"],
-  ["Courses", "+10"],
+  ["DevSkolars", "274"],
+  ["Courses", "8"],
   ["Alumnis", "+500"],
   ["Years", "3"],
-]
+];
 
 export default function Demographics() {
   return (
-    <section className="flex-row px-60 py-[7.5rem] gap-x-[4.56rem] bg-[#8a8a8a]">
-      <div className="text-white">
-        <h2 className="font-heading text-4xl font-bold mb-10">The Devskolar Community</h2>
-        <p className="font-body text-2xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-      </div>
+    <section className="flex flex-row items-center justify-center gap-x-[4.56rem] bg-[#052014] px-60 pb-[164px] pt-60">
+      <div className="flex max-w-[960px] flex-col">
+        <div className="w-[65%] text-white">
+          <h2 className="mb-10 font-heading text-4xl font-bold">The Devskolar Community</h2>
+          <p className="font-body text-xl">
+            The PUP Programmers&apos; Guild is a university wide organization composed of different programmers and
+            developers, conducting different events and activities related to the fields of programming and development.
+          </p>
+        </div>
 
-      <div className="flex-row space-y-10 text-white py-16">
-        {demographics.map(([category, est]) => (
-          <div key={category} className="grid grid-cols-5 gap-x-4">
-            <span className="font-heading text-5xl text-right col-span-4">{est}</span>
-            <span className="font-heading text-[2rem] text-left">{category}</span>
+        <div className="flex flex-1 flex-col">
+          <div className="space-y-10 pt-[72px] text-white">
+            {demographics.map(([category, est]) => (
+              <div key={category} className="grid grid-cols-5 items-end gap-x-4">
+                <span className="col-span-4 text-right font-heading text-5xl">{est}</span>
+                <span className="text-left font-heading text-3xl text-[2rem]">{category}</span>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </section>
-  )
+  );
 }
