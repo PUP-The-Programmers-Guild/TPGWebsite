66b1b7d03ffe51e512a1ea73d644c0fa3c68f9b5
Karmotrine <yuanrcurena@gmail.com>
2024-03-05 17:05:33 +0800

    refactor: impl. mifi design of AboutCommittees
    - rename CommiteeCard.tsx to CommitteeCard.tsx
    - modify section info and content (COMMITTEE_DATA) according to mid-fidelity Figma design
    - modify COMMITTEE_DATA data structure to allow React Element for committeeLogo
    - modify text color, sizing, gap spacing for AboutCommittees section and CommitteeCard element
    - add inner glow through box-shadow to CommitteeCard element
    - add rounded corners to CommitteeCard element
    - adjust spacing for the icon, title, subtitle of CommitteeCard element
    - add green gradient bg to horizontal divider 'div' element inside CommitteeCard element

--- src/components/About/AboutCommittees/AboutCommittees.tsx
+++ src/components/About/AboutCommittees/AboutCommittees.tsx
@@ -1,47 +1,46 @@
-import { CommitteeCard, ICommitteeCardProps } from "./CommiteeCard";
+import { HighlighterCircle, PencilCircle } from "@phosphor-icons/react";
+import { CommitteeCard, ICommitteeCardProps } from "./CommitteeCard";
 
-const COMMITTEE_DATA : ICommitteeCardProps[] = [
-    {
-        committeeTitle: "Media",
-        committeeSubtitle: "The cute name of roles",
-        committeeLogo: "https://via.placeholder.com/50",
-        committeeRoles: ["Social Media", "Graphic Design"],
-        committeeDesc: "Description of the committee Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    },
-    {
-        committeeTitle: "Media",
-        committeeSubtitle: "The cute name of roles",
-        committeeLogo: "https://via.placeholder.com/50",
-        committeeRoles: ["Social Media", "Graphic Design"],
-        committeeDesc: "Description of the committee Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    },
+const COMMITTEE_DATA: ICommitteeCardProps[] = [
+  {
+    committeeTitle: "Media Relations",
+    committeeSubtitle: "(role names?)",
+    committeeLogo: <PencilCircle size={65} weight="fill" />,
+    committeeRoles: ["Social Media", "Media Production"],
+    committeeDesc:
+      "The media committee is in place to address the organization's media concerns within the framework of the existing policies and procedures. Members of the media committee are involved in media-related activities such as broadcasting, publishing, and other internet advertisements. The media head of this committee will serve as a coordinator to help outline content for this committee and outline the responsibilities of each committee member",
+  },
+  {
+    committeeTitle: "Academics",
+    committeeSubtitle: "(role names)",
+    committeeLogo: <HighlighterCircle size={65} weight="fill" />,
+    committeeRoles: ["Researchers", "(need more info)"],
+    committeeDesc:
+      "The academic committee ensures that members' academic interests and queries are raised with the executive committee. Members of the academic committee contribute to the smooth functioning of the educational system of this organization. The academic head ensures that the committee members provide an improved or better academic system for other members.",
+  },
 ];
 
 export default function AboutCommittees() {
-    return (
-        <section
-            className="flex overflow-hidden bg-[#8a8a8a]"
-        >
-            <div className="flex flex-col px-60 mb-40">
-                <h3 className={`text-white text-3xl font-bold`}>The Committees</h3>
-                <p className={`text-white text-xl mt-10`}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-                <div className={`grid grid-cols-2 gap-8 mt-20`}>
-                    {COMMITTEE_DATA.map((committee) => (
-                        <CommitteeCard
-                            key={committee.committeeTitle}
-                            committeeTitle={committee.committeeTitle}
-                            committeeSubtitle={committee.committeeSubtitle}
-                            committeeLogo={committee.committeeLogo}
-                            committeeRoles={committee.committeeRoles}
-                            committeeDesc={committee.committeeDesc}
-                        />
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="flex overflow-hidden bg-[#052014]">
+      <div className="mb-[210px] flex flex-col px-60">
+        <h3 className={`text-3xl font-bold text-white`}>The Committees</h3>
+        <p className={`mt-[40px] text-xl text-white`}>
+          Our Organization consist of two committees that helps the executive committee for efficient operation.
+        </p>
+        <div className={`mt-[78px] grid grid-cols-2 gap-[24px]`}>
+          {COMMITTEE_DATA.map((committee) => (
+            <CommitteeCard
+              key={committee.committeeTitle}
+              committeeTitle={committee.committeeTitle}
+              committeeSubtitle={committee.committeeSubtitle}
+              committeeLogo={committee.committeeLogo}
+              committeeRoles={committee.committeeRoles}
+              committeeDesc={committee.committeeDesc}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
