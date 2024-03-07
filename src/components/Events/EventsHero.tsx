b22df1fe6711f8a7520161f52a0b18b514873c3c
Karmotrine <yuanrcurena@gmail.com>
2024-03-07 23:09:01 +0800

    refactor: impl. mifi design for events hero
    - feat: add EventsHero component to 'events' page
    - fix: change main div display from grid to flex, add centering for text content
    - fix: remove radial-gradient settings for bg-img in main div, change to child div gradient layer
    - fix: change bg-img to TPGMembersBanner.webp
    - fix: add max-width of 960px to text content
    - feat: add min-height calculation that takes account navbar height for the main div
    - fix: adjust vertical gaps between text content
    - fix: adjust x-axis padding for button
    - fix: change bg-color of button from white to tpg-text-green

--- src/components/Events/EventsHero.tsx
+++ src/components/Events/EventsHero.tsx
@@ -1,32 +1,31 @@
 import { ArrowDown } from "@phosphor-icons/react";
 import Image from "next/image";
 
-const BgImageStyle = {
-  zIndex: "-1",
-};
-
 export default function EventsHero() {
   return (
-    <div
-      className="z-100 relative grid min-h-screen content-center px-60"
-      style={{
-        backgroundImage:
-          "radial-gradient(718.21% 485.56% at -44.51% 463.56%,rgba(42, 145, 52, 0.0), rgba(5, 32, 20, 0.85))",
-      }}
-    >
-      <Image src="/news/main2.png" alt="TPG members" fill={true} style={BgImageStyle} />
-      <div className="flex flex-col gap-10">
+    <div className="flex min-h-[calc(100vh-78px)] items-center justify-center">
+      <div className="absolute z-[-20] h-full w-full bg-[radial-gradient(1718.21%_485.56%_at_-44.51%_463.56%,_#2A9134_0%,_#052014_85.26%)]">
+        <Image
+          src="/TPGMembersBanner.webp"
+          alt="TPG members"
+          fill
+          className="z-[-1] h-full w-full object-cover opacity-[35%]"
+        />
+      </div>
+
+      <div className="flex max-w-[960px] flex-col gap-[40px]">
         <div className="font-heading text-5xl font-bold leading-[140%] text-white">
           Building Connections and Experiences through Tech & Beyond
         </div>
-        <div className="flex flex-col gap-20">
+
+        <div className="flex flex-col gap-[90px]">
           <div className="line text-3xl text-white">
             Empower your growth journey with <br />
             TPG&apos;s diverse tech events.
           </div>
-          <button className="text- flex max-w-fit flex-row items-center gap-1 bg-white px-6 py-3">
-            <ArrowDown />
-            Explore Events
+          <button className="flex max-w-fit flex-row items-center gap-1 bg-[#E6F5D6] px-[30px] py-3">
+            <ArrowDown weight="bold" />
+            <span className="text-base font-bold">Explore Events</span>
           </button>
         </div>
       </div>
--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,8 +1,10 @@
 import EventsCatalog from "@/components/Events/EventsCatalog/EventsCatalog";
+import EventsHero from "@/components/Events/EventsHero";
 
 export default function EventsPage() {
   return (
     <>
+      <EventsHero />
       <EventsCatalog />
     </>
   );
