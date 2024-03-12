ee446c12dd0fa2726304b971b0df34a8ba346e9b
Yuan <72153098+Karmotrine@users.noreply.github.com>
2024-03-12 23:50:14 +0800

    refactor: impl. mifi for Benefits comp. (Devskolar) (#76)
    - refactor: move benefit mock data to a separate file (mockBenefits.ts)
    - feat: add proper description for each benefit (source: Mid-fidelity Figma Page)
    - feat: add max-width restriction for benefit section content
    - fix: change section background color
    - fix: change all height/width, font sizing to px unit
    - fix: change button, div background colors
    - fix: adjust benefit card spacing and y-axis gap
    - fix: change display strategy of div for section description, add width limit by 340px
    - fix: change logo sizing to match new font size of button

--- src/components/Devskolar/Benefits/Benefits.tsx
+++ src/components/Devskolar/Benefits/Benefits.tsx
@@ -1,51 +1,39 @@
-import { ArrowRight } from "@phosphor-icons/react";
-
-const benefits = [
-  {
-    title: "Benefit 1",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-  {
-    title: "Benefit 2",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-  {
-    title: "Benefit 3",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-  {
-    title: "Benefit 4",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-];
+import { ArrowUpRight } from "@phosphor-icons/react";
+import { MOCK_BENEFITS } from "@/mock/mockBenefits";
 
 export default function DevskolarBenefits() {
   return (
-    <section className="inline-flex w-full justify-center gap-20 bg-[#8A8A8A] py-10">
-      <div className="flex w-96 flex-col items-start gap-7">
-        <h1 className="font-heading text-4xl font-bold text-white">
-          Devskolar <span className="text-6xl">BENEFITS</span>
-        </h1>
-        <p className="text-xl text-white">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua.
-        </p>
-        <div className="inline-flex items-center gap-2.5 bg-[#f9eeee] px-5 py-2.5">
-          <span className="font-heading text-2xl font-bold text-[#090000]">See Requirements</span>
-          <ArrowRight size={30} />
-        </div>
-      </div>
-      <div className="grid grid-cols-2 gap-5">
-        {benefits.map((benefit, index) => (
-          <div key={index} className="flex h-64 w-64 flex-col gap-y-2.5 bg-[#d9d9d9] p-5 text-center">
-            <h2 className="font-heading text-4xl font-bold text-[#181515]">{benefit.title}</h2>
-            <p className="flex flex-1 items-center text-[#1b1515]">{benefit.description}</p>
+    <section className="flex items-center justify-center bg-[#052014]">
+      <div className="flex max-w-[960px] flex-row gap-x-[70px]">
+        <div className="flex flex-col gap-y-[30px]">
+          <div>
+            <h2 className="font-heading text-4xl font-bold text-white">
+              Devskolar <br /> <span className="text-6xl font-bold leading-[84px]">BENEFITS</span>
+            </h2>
           </div>
-        ))}
+
+          <p className="max-w-[340px] text-lg text-white">
+            Here are some benefits that are dedicated to providing an enriching and rewarding experience during your
+            time as a Devskolar!
+          </p>
+          <div>
+            <button className="flex items-center gap-[10px] bg-[#E6F5D6] px-[20px] py-[10px]">
+              <span className="font-heading text-base font-bold text-[#090000]">See Requirements</span>
+              <ArrowUpRight size={20} />
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 grid-rows-2 gap-[20px]">
+          {MOCK_BENEFITS.map((benefit, index) => (
+            <div key={index} className="flex h-[260px] w-[260px] flex-col gap-y-[10px] bg-white p-[20px] text-center">
+              <h2 className="font-heading text-5xl font-bold text-[#181515]">{benefit.title}</h2>
+              <div className="flex flex-1 items-center justify-center">
+                <p className="text-[16px] text-[#1b1515]">{benefit.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
---
+++ src/mock/mockBenefits.ts
@@ -0,0 +1,22 @@
+export const MOCK_BENEFITS = [
+  {
+    title: "Benefit 1",
+    description:
+      "Feel the sense of community within the TPG as members and officers of the organization gather around through events and assemblies",
+  },
+  {
+    title: "Benefit 2",
+    description:
+      "Develop your technological and leadership skills through the events & activities held within the organization ",
+  },
+  {
+    title: "Benefit 3",
+    description:
+      "Have access to various exclusive PUP-TPG events for members only and be prioritized to our partnered events",
+  },
+  {
+    title: "Benefit 4",
+    description:
+      "Communicate and connect within the organization or through others growing your networks and opportunities",
+  },
+];
