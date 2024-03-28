b5faf4aae8096b5b860d43a9038492d6cad7db74
Karmotrine <yuanrcurena@gmail.com>
2024-03-28 22:28:31 +0800

    fix: phosphoricons SSR issue (#2)
    - fix (index.tsx, events.tsx) Disable SSR for next/dynamic imported ui components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,10 @@ import { IFAQResponse } from "@/lib/types/faq.interface";
 import { IEventCatalogResponse } from "@/lib/types/event.interface";
 
 import HeroCarousel from "@/components/Home/HeroCarousel";
-const Mission = dynamic(() => import("@/components/Home/Mission"), { ssr: true });
-const HomeDemographics = dynamic(() => import("@/components/Home/HomeDemographics"), { ssr: true });
-const LatestNews = dynamic(() => import("@/components/Home/LatestNews/LatestNews"), { ssr: true });
-const FAQS = dynamic(() => import("@/components/Home/FAQS"), { ssr: true });
+const Mission = dynamic(() => import("@/components/Home/Mission"), { ssr: false });
+const HomeDemographics = dynamic(() => import("@/components/Home/HomeDemographics"), { ssr: false });
+const LatestNews = dynamic(() => import("@/components/Home/LatestNews/LatestNews"), { ssr: false });
+const FAQS = dynamic(() => import("@/components/Home/FAQS"), { ssr: false });
 
 export const getServerSideProps = (async (context) => {
   const faqsRes = await fetch(`${process.env.BACKEND_ROOT}/get_faqs`).then((res) => res.json());
