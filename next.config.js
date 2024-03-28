4aa11ee38e8cfa1d12c1ef0695cf41c9cc015a93
Karmotrine <yuanrcurena@gmail.com>
2024-03-28 23:23:01 +0800

    add experimental optimizePackageImports back

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    optimizePackageImports: ["@phosphor-icons/react"],
+  },
   images: {
     remotePatterns: [
       {
