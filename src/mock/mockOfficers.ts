633d222f45e688ac5ea12e8de086d37693726fac
Karmotrine <yuanrcurena@gmail.com>
2024-03-29 16:43:23 +0800

    feat: add dynamic data fetching for Officers comp.
    - feat (Officers.interface.ts) - add separate file for interfaces used by Officers UI components
    - feat (mockOfficers.ts) - add separate file for mock data for Officers component
    - feat (Officers.tsx) - change from mock data to dynamic data fetching
    - feat (about.tsx) - add dynamic data fetching of Officers and Committee Heads via SSR for Officers component

---
+++ src/mock/mockOfficers.ts
@@ -0,0 +1,10 @@
+import { IOfficerInfo } from "@/lib/types/Officers.interface";
+
+const MOCK_OFFICER_INFO: IOfficerInfo = {
+  name: `Full Middle Name Here`,
+  position: `Position Regular`,
+  image_url: `/Placeholder.png`,
+};
+
+//const COMMITTEEHEAD_MOCK_DATA: IOfficerInfo[] = Array(4).fill(MOCK_OFFICER_INFO);
+//const EXECUTIVES_MOCK_DATA: IOfficerInfo[] = Array(20).fill(MOCK_OFFICER_INFO);
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,28 @@
 import Image from "next/image";
 import dynamic from "next/dynamic";
+import { InferGetServerSidePropsType, GetServerSideProps } from "next";
 
 import AboutHero from "@/components/About/AboutHero";
 const MissionVision = dynamic(() => import("@/components/About/MissionVision"), { ssr: true });
 const Demographics = dynamic(() => import("@/components/About/Demographics"), { ssr: true });
 const AboutCommittees = dynamic(() => import("@/components/About/AboutCommittees/AboutCommittees"), { ssr: true });
 const Officers = dynamic(() => import("@/components/About/Officers"), { ssr: true });
+import { IOfficerResponse } from "@/lib/types/Officers.interface";
 const OurAlumnisPage = dynamic(() => import("@/components/About/OurAlumnis"), { ssr: true });
 
 import AboutUsCurlyBraceL from "../../public/AboutUsCurlyBrace-L.webp";
 import AboutUsCurlyBraceR from "../../public/AboutUsCurlyBrace-R.webp";
 
-export default function AboutPage() {
+export const getServerSideProps = (async (context) => {
+  const officerRes = await fetch(`${process.env.BACKEND_ROOT}/get_officers`);
+  let officerData = await officerRes.json();
+  return { props: { officerData } };
+}) satisfies GetServerSideProps<{ officerData: IOfficerResponse }>;
+
+export default function AboutPage({ officerData }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const executivesData = officerData.officers.slice(0, 9);
+  const committeeHeadsData = officerData.officers.slice(9);
+
   return (
     <>
       <AboutHero />
@@ -20,7 +31,7 @@ export default function AboutPage() {
         <div className="relative overflow-hidden">
           <Demographics />
           <AboutCommittees />
-          <Officers />
+          <Officers executivesData={executivesData} committeeHeadsData={committeeHeadsData} />
           <OurAlumnisPage />
           <div
             className="absolute bottom-[-96px] -z-0 select-none stroke-[#343534] stroke-[162px]"
