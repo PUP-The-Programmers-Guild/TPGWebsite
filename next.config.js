bf08342cb59279dd7477a2b558010d1bd5d951aa
Karmotrine <yuanrcurena@gmail.com>
2024-04-03 11:24:19 +0800

    feat: add new imgs, fix event catalog rendering
    - refactor (/public/news): delete unused images
    - refactor (/public, /public/hero, /pages/index.tsx, /pages/about.tsx): add new hero images in carousels
    - feat (_app.tsx, package.json): add "nextjs-progressbar" as page loader
    - fix (event.interface.ts, EventTypeBadge.tsx, EventsCatalog.tsx): fix event catalog rendering (event tags)

--- next.config.js
+++ next.config.js
@@ -1,36 +1,32 @@
+const MillionLint = require('@million/lint');
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === "true"
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizePackageImports: ["@phosphor-icons/react"],
+    optimizePackageImports: ["@phosphor-icons/react"]
   },
   modularizeImports: {
     "@phosphor-icons/react": {
-      transform: "@phosphor-icons/react/dist/ssr/{{member}}",
-    },
+      transform: "@phosphor-icons/react/dist/ssr/{{member}}"
+    }
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: process.env.S3_BUCKET,
-        port: "",
-      },
-    ],
-  },
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "images.unsplash.com",
+      port: ""
+    }, {
+      protocol: "https",
+      hostname: "picsum.photos",
+      port: ""
+    }, {
+      protocol: "https",
+      hostname: process.env.S3_BUCKET,
+      port: ""
+    }]
+  }
 };
-
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = MillionLint.next()(withBundleAnalyzer(nextConfig));
--- src/components/Home/HeroCarousel.tsx
+++ src/components/Home/HeroCarousel.tsx
@@ -2,13 +2,13 @@ import { CaretDown } from "@phosphor-icons/react/dist/ssr/CaretDown";
 import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
 import { useState } from "react";
-import Hero2 from "../../../public/hero/hero2.webp";
 import Link from "next/link";
 
-// Inf. Loop Move, Lazy load
+import Hero1 from "../../../public/hero/Hero1.webp";
+import Hero2 from "../../../public/hero/Hero2.webp";
+import Hero3 from "../../../public/hero/Hero3.webp";
 
-// Mock Data, pictures will be fetched from sanity
-const images = [Hero2, Hero2, Hero2];
+const images = [Hero1, Hero2, Hero3];
 
 export default function HeroCarousel() {
   const [opacities, setOpacities] = useState<number[]>([]);
@@ -16,7 +16,7 @@ export default function HeroCarousel() {
     {
       slides: images.length,
       loop: true,
-      renderMode: "performance",
+      renderMode: "precision",
       detailsChanged(s) {
         const new_opacities = s.track.details.slides.map((slide) => slide.portion);
         setOpacities(new_opacities);
@@ -34,7 +34,7 @@ export default function HeroCarousel() {
           if (mouseOver) return;
           timeout = setTimeout(() => {
             slider.next();
-          }, 2000);
+          }, 2650);
         }
         slider.on("created", () => {
           slider.container.addEventListener("mouseover", () => {
@@ -50,12 +50,13 @@ export default function HeroCarousel() {
         slider.on("dragStarted", clearNextTimeout);
         slider.on("animationEnded", nextTimeout);
         slider.on("updated", nextTimeout);
+        slider.on("destroyed", clearNextTimeout);
       },
     ]
   );
 
   return (
-    <section className="relative min-h-[calc(100vh-80px)]">
+    <section ref={sliderRef} className="relative min-h-[calc(100vh-80px)]">
       <h1 className="absolute left-1/2 top-[calc(50%-80px)] z-10 w-3/5 max-w-4xl -translate-x-1/2 -translate-y-1/2 text-center font-heading text-5xl font-normal uppercase text-[#E6F5D6] lg:text-6xl xl:text-8xl">
         Empowering the next generation of coders
       </h1>
@@ -69,8 +70,8 @@ export default function HeroCarousel() {
         </button>
       </Link>
 
-      <div ref={sliderRef} className="relative -z-10 min-h-screen min-w-full overflow-hidden">
-        <div className="absolute z-0 h-full w-full bg-[linear-gradient(1deg,_#164323_-23.29%,_#052014_64.94%)] opacity-[92%]" />
+      <div className="relative -z-10 min-h-screen min-w-full overflow-hidden">
+        <div className="absolute z-40 h-full w-full bg-[linear-gradient(1deg,_#164323_-23.29%,_#052014_64.94%)] opacity-[90%]" />
         {images.map((src, idx) => (
           <div key={idx} className="absolute -z-10 min-h-full min-w-full" style={{ opacity: opacities[idx] }}>
             <Image
