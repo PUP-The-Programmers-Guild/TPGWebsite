e859d13980daf8715723da27bc69bedc890faa9e
Karmotrine <yuanrcurena@gmail.com>
2024-04-05 13:34:21 +0800

    Modify revalidate path to allow POST method only, change request data from URL query params to json body params

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,44 +1,53 @@
-// https://<FRONTEND_URL>/api/revalidate?path={PATH}/&secret={SECRET}
+/**
+ *    PATH: https://<FRONTEND_URL>/api/revalidate
+ *    METHOD: POST
+ *    INFO: Revalidates the cache of the specified page.
+ *    PAYLOAD: { data: string, secret: string }
+ */
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.query.secret !== process.env.REVALIDATION_SECRET) {
-    return res.status(401).json({ message: "Unauthorized token." });
-  }
+  if (req.method === "POST") {
+    const { data, secret } = JSON.parse(req.body);
+    if (secret !== process.env.REVALIDATION_SECRET) {
+      return res.status(401).json({ message: "Unauthorized secret token." });
+    }
 
-  const data = req.query.data as string;
-  switch (data) {
-    case "events":
-      await res
-        .revalidate("/events")
-        .then(async () => {
-          await res.revalidate("/events/[id]").catch((err) => {
+    switch (data) {
+      case "events":
+        await res
+          .revalidate("/events")
+          .then(async () => {
+            await res.revalidate("/events/[id]").catch((err) => {
+              res.status(500).json({ message: err.message });
+            });
+          })
+          .catch((err) => {
             res.status(500).json({ message: err.message });
           });
-        })
-        .catch((err) => {
+        break;
+      case "faqs":
+        await res.revalidate("/").catch((err) => {
           res.status(500).json({ message: err.message });
         });
-      break;
-    case "faqs":
-      await res.revalidate("/").catch((err) => {
-        res.status(500).json({ message: err.message });
-      });
-      break;
-    case "officers":
-      await res.revalidate("/about").catch((err) => {
-        res.status(500).json({ message: err.message });
-      });
-      break;
-    case "alumni":
-      await res.revalidate("about").catch((err) => {
-        res.status(500).json({ message: err.message });
-      });
-      break;
-    default:
-      return res.status(422).json({
-        message: "Invalid dataType in '?data={}' provided. Please check if dataType is in plural form.",
-      });
+        break;
+      case "officers":
+        await res.revalidate("/about").catch((err) => {
+          res.status(500).json({ message: err.message });
+        });
+        break;
+      case "alumni":
+        await res.revalidate("about").catch((err) => {
+          res.status(500).json({ message: err.message });
+        });
+        break;
+      default:
+        return res.status(422).json({
+          message: "Invalid dataType provided in 'data' field.",
+        });
+    }
+    return res.json({ revalidated: true });
+  } else {
+    return res.status(405).json({ message: "Method not allowed." });
   }
-  return res.json({ revalidated: true });
 }
