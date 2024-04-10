3cbb2c74c6f3ad2b5ecfba793c51cbcad473eac9
Yuan <72153098+Karmotrine@users.noreply.github.com>
2024-04-10 14:20:23 +0800

    feat: add adviser info to about us page (#86)
    * feat: add adviser info to about us page
    
    - feat (Officers.tsx): add adviser info to about us page
    - feat (Officers.tsx): change Header text from "The Officers" to "The Team"
    
    * Hotfix: Remove 'Position' text for Adviser Card

--- src/components/About/Officers.tsx
+++ src/components/About/Officers.tsx
@@ -2,15 +2,24 @@ import { IOfficerOffice, IOfficerInfo, IOfficersComponentProps } from "@/lib/typ
 import Image from "next/image";
 
 export default function Officers({ executivesData, committeeHeadsData }: IOfficersComponentProps) {
+  const ADVISER_INFO: IOfficerInfo = {
+    name: "Sir John Dustin Santos",
+    position: "",
+    image_url: "/Placeholder.png",
+  };
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#052014]">
-      <div className="flex max-w-[960px] flex-col pb-[210px]">
-        <div className="z-10 pb-[80px] ">
-          <h1 className="pb-[80px] font-heading text-[2.5rem] font-semibold text-white">The Officers</h1>
-          <h2 className="pb-[40px] font-heading text-[2rem] font-semibold text-white">Our Executives</h2>
-          <p className="pb-[40px] font-body text-xl text-white">
-            These are the student leaders that make the organization work.
-          </p>
+      <div className="flex max-w-[960px] flex-col gap-y-[80px] pb-[210px]">
+        <h1 className=" font-heading text-3xl font-semibold text-white">The Team</h1>
+
+        <div className="z-10 flex flex-col gap-y-[40px]">
+          <h2 className="font-heading text-2xl font-semibold text-white">Our Adviser</h2>
+          <OfficerInfoCard {...ADVISER_INFO} key="adviser" />
+        </div>
+
+        <div className="z-10 flex flex-col gap-y-[40px]">
+          <h2 className="font-heading text-2xl font-semibold text-white">Our Executives</h2>
+          <p className="font-body text-lg text-white">These are the student leaders that make the organization work.</p>
 
           <ul role="list" className="z-10 grid grid-flow-row grid-cols-4 gap-x-[46.33px] gap-y-[32px]">
             {executivesData.map((office, idx) => {
@@ -21,20 +30,18 @@ export default function Officers({ executivesData, committeeHeadsData }: IOffice
           </ul>
         </div>
 
-        <div className="z-10">
-          <h2 className="pb-[40px] font-heading text-[2rem] font-semibold text-white">Our Committee Heads</h2>
-          <p className="pb-[40px] font-body text-xl text-white">
-            These are the student leaders that handles the committees.
-          </p>
-        </div>
+        <div className="z-10 flex flex-col gap-y-[40px]">
+          <h2 className="font-heading text-2xl font-semibold text-white">Our Committee Heads</h2>
+          <p className="font-body text-lg text-white">These are the student leaders that handles the committees.</p>
 
-        <ul role="list" className="z-10 grid grid-flow-row auto-rows-max grid-cols-4 gap-x-[46.33px] gap-y-[32px]">
-          {committeeHeadsData.map((office, idx) => {
-            return office.officers.map((officer, idx) => (
-              <OfficerInfoCard {...officer} key={`${officer.position}-${idx}`} />
-            ));
-          })}
-        </ul>
+          <ul role="list" className="z-10 grid grid-flow-row auto-rows-max grid-cols-4 gap-x-[46.33px] gap-y-[32px]">
+            {committeeHeadsData.map((office, idx) => {
+              return office.officers.map((officer, idx) => (
+                <OfficerInfoCard {...officer} key={`${officer.position}-${idx}`} />
+              ));
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
@@ -45,7 +52,7 @@ function OfficerInfoCard(props: IOfficerInfo, idx: number) {
     <li key={`${props.name}-${idx}`} className="flex gap-x-[15px] overflow-clip whitespace-pre-line break-words">
       <Image
         className="self-center rounded-full"
-        src={props.image_url ? props.image_url : `/Placeholder.png`}
+        src={props?.image_url ? props.image_url : `/Placeholder.png`}
         alt="placeholder"
         width={64}
         height={64}
