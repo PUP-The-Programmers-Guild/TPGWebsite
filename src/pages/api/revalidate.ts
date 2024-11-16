6a33c229d91c2e9c42912ea11ab78b347851cf09
Yuan <72153098+Karmotrine@users.noreply.github.com>
2024-11-16 22:16:57 +0800

    refactor: cache revalidate route (#92)

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -7,53 +7,42 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const jsonBody = await JSON.parse(req.body);
-    if (!jsonBody.data || !jsonBody.secret) {
-      return res.status(422).json({ message: "Missing required fields." });
-    }
-    const { data, secret } = jsonBody;
-    if (secret !== process.env.REVALIDATION_SECRET) {
-      return res.status(401).json({ message: "Unauthorized secret token." });
-    }
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed." });
+  }
+  const { data, secret } = await JSON.parse(req.body);
+  if (!data || !secret) {
+    return res.status(422).json({ message: "Missing required fields." });
+  }
 
+  if (secret !== process.env.REVALIDATION_SECRET) {
+    return res.status(401).json({ message: "Unauthorized secret token." });
+  }
+  try {
     switch (data) {
       case "events":
-        await res
-          .revalidate("/events")
-          .then(async () => {
-            await res.revalidate("/").catch((err) => {
-              res.status(500).json({ message: err.message });
-            });
-          })
-          .catch((err) => {
-            res.status(500).json({ message: err.message });
-          });
+        await res.revalidate("/events").then(async () => {
+          await res.revalidate("/");
+        });
         break;
       case "faqs":
-        await res.revalidate("/").catch((err) => {
-          res.status(500).json({ message: err.message });
-        });
-        break;
-      case "officers":
-        await res.revalidate("/about").catch((err) => {
-          res.status(500).json({ message: err.message });
-        });
+        await res.revalidate("/");
         break;
       case "alumni":
-        await res.revalidate("/about").catch((err) => {
-          res.status(500).json({ message: err.message });
-        });
+        await res.revalidate("/about");
+        break;
+      case "officers":
+        await res.revalidate("/about");
         break;
       default:
         return res.status(422).json({
           message: "Invalid dataType provided in 'data' field.",
         });
     }
-    return res.json({ revalidated: true });
-  } else if (req.method === "GET") {
-    return res.status(405).json({ message: "GET Method not allowed." });
-  } else {
-    return res.status(405).json({ message: "Method not allowed." });
+    return res.json({ revalidated: true, message: "Cache Revalidated Successfully" });
+  } catch (error) {
+    return res.status(500).json({
+      message: error instanceof Error ? error.message : "An unknown error occurred.",
+    });
   }
 }
